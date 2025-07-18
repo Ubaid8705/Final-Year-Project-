c139fa4ca620f0700f7f5ba87b9c4549aa0546bc
Bilal Qamar <bilalqamar229@gmail.com>
2025-07-18 21:14:10 +0500

    header complete

--- frontend/src/Components/header.jsx
+++ frontend/src/Components/header.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from 'react';
 import './header.css';
 
 function Header() {
   return (
     <div className="header">
-        <h1>My React Header</h1>
-        <div className='search'>
-            <input type="search" name="" id="" />
-        </div>
+      <div className="logo">Medium</div>
+      <div className="search-bar">
+        <span className="search-icon">&#128269;</span>
+        <input type="search" placeholder="Search" />
+      </div>
+      <div className="header-actions">
+        <button className="write-btn">
+          <span className="write-icon">&#9998;</span> Write
+        </button>
+        <span className="bell-icon">&#128276;</span>
+        <img
+          className="avatar"
+          src="https://randomuser.me/api/portraits/men/32.jpg"
+          alt="avatar"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
